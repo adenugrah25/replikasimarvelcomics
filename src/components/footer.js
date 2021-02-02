3d36da66e5b2f0bb00d336d853339ba6febc01d3
adenugrah25 <adenugrah25@gmail.com>
2021-02-02 11:45:47 +0700

    bug-fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,18 @@
 import React from 'react'
-// import TwitterIcon from '@material-ui/icons/Twitter';
-// import InstagramIcon from '@material-ui/icons/Instagram';
-// import FacebookIcon from '@material-ui/icons/Facebook';
-// import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Footer () {
     return (
         <div style={styles.root}>
             <h1 style={styles.copyright}>Copyright 2021 © @adenugrah25</h1>
             <ul style={styles.ul}>
-                <li style={styles.li}><span class="material-icons">facebook</span></li>
-                {/* <li style={styles.li}><InstagramIcon fontSize="small"/></li>
+                <li style={styles.li}><FacebookIcon fontSize="small"/></li>
+                <li style={styles.li}><InstagramIcon fontSize="small"/></li>
                 <li style={styles.li}><TwitterIcon fontSize="small"/></li>
-                <li style={styles.li}><GitHubIcon fontSize="small"/></li> */}
+                <li style={styles.li}><GitHubIcon fontSize="small"/></li>
             </ul>
         </div>
     )
@@ -23,10 +23,10 @@ const styles = {
         height : 70,
         width : '100%',
         // backgroundColor : '#130f40',
-        // backgroundColor : 'rgba(30, 39, 46, 0.3)',
-        backgroundColor: "#f2f2f2",
+        backgroundColor : 'rgba(30, 39, 46, 0.3)',
+        // backgroundColor: "#f2f2f2",
         // backgroundColor : '#48C9B0',
-        padding : '2% 7%',
+        // padding : '2% 3%',
         display : 'flex',
         justifyContent : 'space-between',
         alignItems : 'center'
@@ -34,7 +34,7 @@ const styles = {
 
     copyright : {
         fontSize : 16,
-        color : 'blue'
+        color : 'black'
     },
 
     ul : {
@@ -43,7 +43,7 @@ const styles = {
 
     li : {
         display : 'inline-block',
-        color : 'white',
+        color : 'black',
         marginLeft : 15,
         cursor : 'pointer'
     }
--- src/pages/content.js
+++ src/pages/content.js
@@ -8,16 +8,9 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-// import GridListTileBar from "@material-ui/core/GridListTileBar";
-// import ListSubheader from "@material-ui/core/ListSubheader";
-
-// import IconButton from "@material-ui/core/IconButton";
-// import InfoIcon from "@material-ui/icons/Info";
-// import { Grid } from "@material-ui/core";
 
 class Content extends React.Component {
   constructor(props) {
@@ -98,7 +91,7 @@ const styles = {
     height: "auto",
     width: "100%",
     backgroundColor: "#f2f2f2",
-    padding: "2% 7%",
+    padding: "2% 5%",
   },
   title: {
     fontSize: 50,
